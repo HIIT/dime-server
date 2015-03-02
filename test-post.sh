9671daad9c5a154e24dc24bda44adfc6ff177a3e
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2015-03-02 10:22:01 +0200

    Some cleanup.

--- test-post.sh
+++ test-post.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -X POST -H "Content-Type: application/json" http://localhost:8080/logger -d @json
+curl -X POST -H "Content-Type: application/json" http://localhost:8080/logger \
+    -d '{ "name": "BarFoo", "eventType": "created", "time": "2015-03-02T08:11:00Z" }'
 echo
