38dd1ef976a22e79752d5e1da6c2899d60713213
Markus Koskela <markus.koskela@cs.helsinki.fi>
2015-03-12 16:23:01 +0200

    Dict for .desktop files to Application names.

--- zg2dime.py
+++ zg2dime.py
@@ -10,7 +10,7 @@ import hashlib
 import urllib
 
 from gi.repository import GLib
-
+from ConfigParser import SafeConfigParser
 from zeitgeist.client import ZeitgeistClient
 from zeitgeist.datamodel import *
 
@@ -20,6 +20,43 @@ def json_to_md5(payload):
     md5.update(json_payload)
     return md5.hexdigest()
  
+def _get_app_paths():
+    paths = os.environ.get('XDG_DATA_HOME', 
+        os.path.expanduser('~/.local/share/')).split(os.path.pathsep)
+    paths.extend(os.environ.get('XDG_DATA_DIRS', 
+        '/usr/local/share/:/usr/share/').split(os.path.pathsep))
+    return paths
+
+def locate_desktop_file(filename, _paths=_get_app_paths()):
+    print filename
+    for path in _paths:
+        for thispath, dirs, files in os.walk(os.path.join(path, 'applications')):
+            if filename not in files:
+                continue
+            fullname = os.path.join(thispath, filename)
+            print fullname
+            return fullname
+    else:
+        raise IOError
+
+def map_actor(actor):
+    if actor.startswith('application://'):
+        actor = actor[14:]
+    if actors.has_key(actor):
+        return actors[actor]
+    try:
+        desktop_file = locate_desktop_file(actor)
+    except IOError:
+        actors[actor] = actor
+        return actors[actor]
+    parser = SafeConfigParser()
+    parser.read(desktop_file)
+    if parser.has_section('Desktop Entry') and parser.has_option('Desktop Entry', 'Name'):
+        actors[actor] = parser.get('Desktop Entry', 'Name')
+    else:
+        actors[actor] = actor
+    return actors[actor]
+
 def send_event(event):
 
     storage = 'deleted'
@@ -40,7 +77,7 @@ def send_event(event):
             text = text.rstrip()
 
     payload = {'origin':                 hostname,
-               'actor':                  event.actor, 
+               'actor':                  map_actor(event.actor), 
                'interpretation':         event.interpretation,
                'manifestation':          event.manifestation,               
                'timestamp':              event.timestamp,
@@ -74,14 +111,25 @@ def on_events_received(events):
 def on_delete(time_range, event_ids):
     print event_ids
 
-def main():
-    GLib.MainLoop().run()
+def foo():
+    print "foo"
+    return True
+
+nevents=20
+if len(sys.argv)>1:
+    if sys.argv[-1] == 'debug':
+        server_url = 'http://httpbin.org/post'
+        nevents = 1
+    elif sys.argv[-1] == 'all':
+        nevents = 1000
+
+actors = {'acroread.desktop':'Adobe Reader', 'kde4-okular.desktop':'Okular'}
 
 zeitgeist = ZeitgeistClient()
  
 template = Event.new_for_values(subject_interpretation=Interpretation.DOCUMENT)
 
-zeitgeist.find_events_for_template(template, on_events_received, num_events=100)
+zeitgeist.find_events_for_template(template, on_events_received, num_events=nevents)
 zeitgeist.install_monitor(TimeRange.always(), [template], on_insert, on_delete)
 
 hostname = socket.gethostbyaddr(socket.gethostname())[0]
@@ -91,12 +139,10 @@ uuid = uuid.rstrip()
 
 server_url = 'http://localhost:8080/api/data/zgevent'
 
-if len(sys.argv)>0 and sys.argv[-1] == 'debug':
-    server_url = 'http://httpbin.org/post'
-
 if __name__ == '__main__':
     try:
-        main()
+        GLib.timeout_add(60*1000, foo)
+        GLib.MainLoop().run()
     except KeyboardInterrupt:
         print("Exiting")
 
