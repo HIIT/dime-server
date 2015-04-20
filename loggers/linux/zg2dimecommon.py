9f4ebf5bc525784d60d9ab8a08b1b5c1f4c39c32
Markus Koskela <markus.koskela@cs.helsinki.fi>
2015-04-20 16:33:56 +0300

    New JSON functions.

--- loggers/linux/zg2dimecommon.py
+++ loggers/linux/zg2dimecommon.py
@@ -8,7 +8,8 @@ from zg2dimeglobals import config
 
 # -----------------------------------------------------------------------
 
-def json_to_sha1(payload):
+def to_json_sha1(payload):
+    """Convert to JSON and return a SHA-1 digest."""     
     json_payload = json.dumps(payload)
     sha1 = hashlib.sha1()
     sha1.update(json_payload)
@@ -16,12 +17,42 @@ def json_to_sha1(payload):
 
 # -----------------------------------------------------------------------
 
-def json_dumps(text):
-    return json.dumps(text)
+def str_to_sha1(json_payload):
+    """Return a SHA-1 digest for a string."""     
+    sha1 = hashlib.sha1()
+    sha1.update(json_payload)
+    return sha1.hexdigest()
+
+# -----------------------------------------------------------------------
+
+def json_dumps(obj):
+    """Convert (serialize) to JSON."""
+    return json.dumps(obj)
+
+# -----------------------------------------------------------------------
+
+def json_dump(obj, fn):
+    """Save as JSON."""
+    with open(fn, 'w') as fp: 
+        json.dump(obj, fp)
+
+# -----------------------------------------------------------------------
+
+def json_load(fn):
+    """Load JSON from file object."""
+    with open(fn, 'r') as fp: 
+        return json.load(fp)
+
+# -----------------------------------------------------------------------
+
+def json_loads(text):
+    """Load JSON from string."""
+    return json.loads(text)
 
 # -----------------------------------------------------------------------
 
 def ping_server():
+    """Ping DiMe, returns boolean for success."""
     try:
         r = requests.post(config['server_url']+'/ping')
         #print r.text
@@ -32,6 +63,7 @@ def ping_server():
 # -----------------------------------------------------------------------
 
 def post_json(payload):
+    """HTTP POST JSON-format payload to DiMe."""
     headers = {'content-type': 'application/json'}
     return requests.post(config['server_url']+"/data/zgevent",
                          data=payload,
