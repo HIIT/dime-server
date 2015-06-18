1661223047ff23e754ba3a1e26beddbc7e50faaa
Petri Luukkonen <petri.luukkonen@helsinki.fi>
2015-06-18 16:31:27 +0300

    The latest version of keylogger (including user.ini file)

--- loggers/keylogger/keylogger.py
+++ loggers/keylogger/keylogger.py
@@ -35,7 +35,6 @@ from PyQt4.QtCore import *
 #
 import re
 
-
 class WindowClass(QtGui.QMainWindow):
 #class WindowClass(QtGui.QWidget):
     
@@ -43,7 +42,7 @@ class WindowClass(QtGui.QMainWindow):
       super(WindowClass, self).__init__()
 
       #Set the window title
-      self.setWindowTitle('Keylogger')
+      self.setWindowTitle('Proactive Search')
 
       #Always on top
       self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
@@ -53,25 +52,44 @@ class WindowClass(QtGui.QMainWindow):
       filemenu = menubar.addMenu('&File')
       filemenu.addAction('Quit', self.quitting)
 
-      #Bind the event handlers to the button
+      #Quit button
       self.quitbutton = QtGui.QPushButton("Quit",self)
       self.quitbutton.resize(40,20)
       self.quitbutton.clicked.connect(self.quitting)
       self.quitbutton.move(270,7)
-      #self.quitbutton.move(50,5)
 
+      #Stop/Start button
+      ssbuttonstr = "Stop logging"
+      self.ssbutton = QtGui.QPushButton(ssbuttonstr,self)
+      self.ssbutton.setStyleSheet("color: red")
+      self.ssbutton.resize(90,20)
+      self.ssbutton.clicked.connect(self.stopstart)
+      self.ssbutton.move(10,7)
 
       #Set color of labels
       palette = QtGui.QPalette()
       palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.darkGreen)
 
       #Create QGroupbox
-      self.mygroupbox = QtGui.QGroupBox("Links to suggested resources:",self)
+      self.mygroupbox = QtGui.QGroupBox()
       #Create layout
       self.myform = QtGui.QFormLayout()
+      #Add self.myform to self.mygroupbox layout
+      self.mygroupbox.setLayout(self.myform)
+
+      #Make labellist
+      self.labellist = []
+
+      #Create status label    
+      self.statuslabel = QtGui.QLabel("Logging ongoing")
+      self.statuslabel.setStyleSheet("color: darkGreen")
+      self.myform.addRow(self.statuslabel)
+
+      #Create infolabel    
+      self.infolabel = QtGui.QLabel("Links to suggested resources:")
+      self.myform.addRow(self.infolabel)
 
       #Make clickable labels
-      self.labellist = []
       for i in range(5):
         #
         dumlabel = ExtendedQLabel.ExtendedQLabel("", self)
@@ -81,9 +99,6 @@ class WindowClass(QtGui.QMainWindow):
 	self.labellist.append(dumlabel) 
 	#
 	self.myform.addRow(self.labellist[i])
-
-      #Add self.myform to self.mygroupbox
-      self.mygroupbox.setLayout(self.myform)
 
       #Create scrollbar area
       self.scrollArea = QtGui.QScrollArea(self)
@@ -99,6 +114,9 @@ class WindowClass(QtGui.QMainWindow):
       #Get the current path
       self.pathstr = os.path.dirname(os.path.realpath(sys.argv[0]))
 
+      #Read user.ini file
+      self.username, self.password, self.time_interval, self.nspaces = self.read_user_ini()
+
       #Initial time
       self.cdate          = str(datetime.datetime.now().date())
       self.time           = str(datetime.datetime.now().time())
@@ -106,9 +124,6 @@ class WindowClass(QtGui.QMainWindow):
       self.done           = False
       #
       self.sleep_interval = 1.005
-
-    def closeEvent(self, event):
-        self.quitting()
 
     def startlog(self):
         threading.Thread(target = log, args = (self,) ).start()
@@ -119,7 +134,7 @@ class WindowClass(QtGui.QMainWindow):
     def update_links(self, urlstr):
         #pass
         i = 0
-        sleep(2.0)
+        #sleep(2.0)
         #for i in len(r.json())
         if urlstr != None:
                 for i in range( len(urlstr.json()) ):
@@ -130,14 +145,59 @@ class WindowClass(QtGui.QMainWindow):
                                 self.labellist[i].setToolTip(tooltipstr[0:120])
 
 
+    def read_user_ini(self):
+
+        f          = open('user.ini','r')
+        dumstr     = f.read()
+        stringlist = dumstr.split()
+
+        for i in range( len(stringlist) ):
+                if stringlist[i] == "usrname:":
+                        usrname = stringlist[i+1]
+                if stringlist[i] == "password:":
+                        password = stringlist[i+1]
+                if stringlist[i] == "time_interval:":
+                        time_interval_string = stringlist[i+1]
+                        time_interval = float(time_interval_string)
+                if stringlist[i] == "nspaces:":
+                        nspaces_string = stringlist[i+1]
+                        nspaces = int(nspaces_string)
+
+        return usrname, password, time_interval, nspaces
+
+    #
     def open_url(self):
       global urlstr
       webbrowser.open(urlstr)
+
+    #
+    def stopstart(self):
+      global var
+
+      if var == True:
+	      var = False
+	      self.ssbutton.setText("Start logging")
+	      self.ssbutton.setStyleSheet("background-color: lightGreen")
+	      self.statuslabel.setText("Logging disabled")
+	      self.statuslabel.setStyleSheet("color: red")
+      elif var == False:
+	      var = True
+	      self.ssbutton.setText("Stop logging")
+	      self.ssbutton.setStyleSheet("color: red")
+	      self.statuslabel.setText("Logging ongoing")
+	      self.statuslabel.setStyleSheet("color: green")
+	      #self.statuslabel.setStyleSheet("background-color: lightRed")
+              self.startlog()
 
     def quitting(self):
       global var
       var = False
       QtCore.QCoreApplication.instance().quit()
+
+    #Quit
+    def closeEvent(self, event):
+        self.quitting()
+
 
 ################################################################
 
@@ -324,7 +384,8 @@ def fetch_keys():
 ###
 def log(win):
 
-      global urlstr
+      #global urlstr
+      global var
 
       countspaces = 0
       sleep_interval = 0.005
@@ -335,14 +396,13 @@ def log(win):
       flag = 0
       flag2= 0
       dumstr = ''
-
+      wordlist = []
       #Show the links of suggested resources in the window
       #update_kurllabel2(self)
 
-      f = open('typedwords.txt', 'a')
+      #f = open('typedwords.txt', 'a')
       while var:
 
-        strdum = ''
         sleep(sleep_interval)
         changed, modifiers, keys = fetch_keys()
         keys  = str(keys)
@@ -352,13 +412,13 @@ def log(win):
         cdate = datetime.datetime.now().date()
         ctime = datetime.datetime.now().time()
 
-	cmachtime = time()
-        var2 = cmachtime > now + timeinterval
+        cmachtime = time()
+        var2 = cmachtime > now + win.time_interval
 
         if keys == 'None':
                 keys = ''
-        
-	elif keys == '<backspace>':
+
+        elif keys == '<backspace>':
                 keys = ''
                 #Convert current string into list of characters
                 duml = list(dumstr)
@@ -368,78 +428,63 @@ def log(win):
                         #Convert back to string
                         dumstr = "".join(duml)
 	
-	elif keys in ['<enter>', '<tab>','<right ctrl>','<left ctrl>'," "]:
-		keys = ' '
-		dumstr = dumstr + keys
-		countspaces = countspaces + 1
+        elif keys in ['<enter>', '<tab>','<right ctrl>','<left ctrl>'," "]:
+
+        	#keys = ' '
+	        wordlist.append(dumstr)
+                #dumstr = dumstr + keys
+	        countspaces = countspaces + 1
+	        dumstr = ''
+	
+		if var2:
+              		#
+			dumstr2 = ''
+        	        for i in range( len(wordlist) ):
+                 	       dumstr2 = dumstr2 + wordlist[i] + ' '
+
+	                #
+                        f = open("typedwords.txt","a")
+        	        f.write(str(cdate) + ' ' + str(ctime) + ' ' + dumstr2 + '\n')
+		        f.close()
+
+                	#Make query to DiMe
+	                urlstr = search_dime(win.username, win.password, dumstr2)
+        	        #Update visible links
+                	win.update_links(urlstr)
+
+	                #Add the suggested url into a history file
+        	        if urlstr != None:
+                	        f2 = open("suggested_pages.txt","a")
+                        	f2.write(str(cdate) + ' ' + str(ctime) + ' ' + str(urlstr.json()[0]["uri"]) + '\n')
+	                        f2.close()
+
+        	        #Clear the dummy string
+                	dumstr = ''
+	                dumstr2= ''
+	
+        	        #Remove content from wordlist
+                	del wordlist[:]
+
+	                countspaces = 0
+
+        	        flag = 1
+                	flag2= 0
+
+	                now = time()
+
         else:
                 cdate = datetime.datetime.now().date()
                 ctime = datetime.datetime.now().time()
                 dumstr = dumstr + keys
-        
-	if var2 or countspaces == 5:
-                f.write(str(cdate) + ' ' + str(ctime) + ' ' + dumstr + '\n')
-
-                #Make query to DiMe
-                urlstr = search_dime(dumstr)
-		#Update visible links
-		win.update_links(urlstr)
-	
-		#Updates 
-		#print "foo"
-		#print countspaces
-                #print urlstr
-
-                #Add the suggested url into a history file
-                if urlstr != None:
-	                f2 = open("suggested_pages.txt","a")
-                	f2.write(str(cdate) + ' ' + str(ctime) + ' ' + str(urlstr.json()[0]["uri"]) + '\n')
-                        f2.close()
-
-                #Clear the dummy string
-                dumstr = ''
-		countspaces = 0
-
-                flag = 1
-                flag2= 0
-
-		now = time()
 
 
 
-      f.close()
-
-
-
-def update_visible_link(win):
-      i = 0
-      global urlstr
-      global var
-
-      while var:
-        sleep(2.0)
-        #for i in len(r.json())
-	if urlstr != None:
-		for i in range( len(urlstr.json()) ):
-				linkstr = str( urlstr.json()[i]["uri"] )
-				win.labellist[i].setText(linkstr)
-				#self.pathlabel.setText('<i>hello</i>')
-				#self.pathlabel.setText('<a href="{0}">{0}</a>'.format(linkstr))
-				plaintext    = urlstr.json()[i]["plainTextContent"]
-				tooltipstr   = re.sub("[^\w]", " ", plaintext)
-				#print tooltipstr1[0:120]
-				win.labellist[i].setToolTip(tooltipstr[0:120])
-
-
-def search_dime(query):
-#def search_dime(server_username, server_password, query)
+def search_dime(username, password, query):
 	#------------------------------------------------------------------------------
 
 	server_url = 'http://localhost:8080/api'
-	server_username = 'jmakoske'
-	server_password = '0yI6QrnYI63ZvBLde1y4'
-#	server_username = 'petrihiit'
-#	server_password = 'p3tr1h11t'
+	server_username = str(username)
+	server_password = str(password)
 
 	#------------------------------------------------------------------------------
 
@@ -453,13 +498,13 @@ def search_dime(query):
 	# make search query
 	#query = "dime"
 	#query = "python"
-	print query
 
 	r = requests.get(server_url + '/search?query={}&limit=5'.format(query),
         	         headers={'content-type': 'application/json'},
                 	 auth=(server_username, server_password),
 	                 timeout=10)
 	
+	print query
 	print len(r.json())
 
 	if len(r.json()) > 0:
@@ -476,7 +521,7 @@ if __name__ == "__main__":
   #Important global variables!
   global urlstr
   #Initialize urlstr
-  urlstr = search_dime("python")
+  urlstr = search_dime("petrihiit","p3tr1h11t","python")
 
   global var
   var = True
@@ -498,14 +543,8 @@ if __name__ == "__main__":
   #Move the window into the right corner
   newwindow.move(sl-350,0)
 
-  #Start the url updating 
-  #newwindow.show_link()  
-
   #Start keylogger
   newwindow.startlog()
 
   #Start
   app.exec_()
-
-  #
-  #sys.exit(app.exec_())
