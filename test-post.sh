72787cb2468d8e513febd908ab6e8a2ba8adffc8
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2015-08-10 17:05:28 +0300

    Updated remaining example scripts to work with new API

--- test-post.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -X POST -H "Content-Type: application/json" -u mats http://localhost:8080/api/data/zgevent \
-    -d '{ "actor": "Emacs", "manifestation": "something", "timestamp": "2015-03-02T08:11:00Z" }'
-
-echo
