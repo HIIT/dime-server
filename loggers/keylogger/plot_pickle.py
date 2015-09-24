1d162afd53d449ead7710871dd21f0681dab15df
Markus Koskela <markus.koskela@cs.helsinki.fi>
2015-09-24 10:51:15 +0300

    Added plotting of mean vector.

--- loggers/keylogger/plot_pickle.py
+++ loggers/keylogger/plot_pickle.py
@@ -6,11 +6,25 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
+means = []
+for i in range(0,500):
+    means.append([])
+
 for a in sys.argv[1:]:
-    print a 
+    print "Processing", a 
     f = open(a) 
     x = pickle.load(f)
+    for i,xx in enumerate(x):
+        if i<500:
+            print i,xx
+            means[i].append(xx[0])
     y = np.array(x)
     plt.plot(y[:,0])
 
+meanvector = np.zeros(500)
+for i,m in enumerate(means):
+    print i, len(m), type(m), m[0]
+    meanvector[i] = float(sum(m))/len(m)
+plt.plot(meanvector, linewidth=5, color='k')
+    
 plt.show()
