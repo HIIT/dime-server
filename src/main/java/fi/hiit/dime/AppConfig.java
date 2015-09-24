8c01114104dc0e3470bd00d5d37d7f5b117ae9eb
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2015-09-24 17:15:14 +0300

    Initial Bean configs for Lucene support

--- src/main/java/fi/hiit/dime/AppConfig.java
+++ src/main/java/fi/hiit/dime/AppConfig.java
@@ -24,20 +24,37 @@
 
 package fi.hiit.dime;
 
-//------------------------------------------------------------------------------
-
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import org.springframework.context.annotation.Configuration;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexNotFoundException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-//------------------------------------------------------------------------------
+import java.nio.file.Paths;
+import java.io.IOException;
 
 @Configuration
 public class AppConfig {
-    public static final String DB_NAME = "dime";
+    private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
 
+    // MongoDB configuration
+
+    public static final String DB_NAME = "dime";
 
     public @Bean Mongo mongo() throws Exception {
 	return new MongoClient("localhost");
@@ -45,5 +62,69 @@ public class AppConfig {
     
     public @Bean MongoTemplate mongoTemplate() throws Exception {
 	return new MongoTemplate(mongo(), DB_NAME);
+    }
+
+
+    // Lucene configuration
+
+    // @Value("#{appProperties['lucene.index_path']}")
+    private String indexPath = "/home/mvsjober/var/dime-lucene";
+
+    @Bean(name="analyzer")
+    public Analyzer getAnalyzer() {
+        return new StandardAnalyzer();
+    }
+
+    @Bean(name="fsDirectory")
+    @DependsOn("analyzer")
+    public FSDirectory getFSDirectory() throws IOException {
+        return FSDirectory.open(Paths.get(indexPath)); //, new NativeFSLockFactory());
+    }
+
+    @Bean(name="indexWriter")
+    @DependsOn("fsDirectory")
+    public IndexWriter getIndexWriter() throws IOException {
+        IndexWriterConfig iwc = new IndexWriterConfig(getAnalyzer());
+        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+
+	// Advice from Lucene example:
+	// http://lucene.apache.org/core/5_3_0/demo/src-html/org/apache/lucene/demo/IndexFiles.html
+	// Optional: for better indexing performance, if you
+	// are indexing many documents, increase the RAM
+	// buffer.  But if you do this, increase the max heap
+	// size to the JVM (eg add -Xmx512m or -Xmx1g):
+	//
+	// iwc.setRAMBufferSizeMB(256.0);
+
+        IndexWriter writer = new IndexWriter(getFSDirectory(), iwc);
+
+	// NOTE: if you want to maximize search performance,
+	// you can optionally call forceMerge here.  This can be
+	// a terribly costly operation, so generally it's only
+	// worth it when your index is relatively static (ie
+	// you're done adding documents to it):
+	//
+	// writer.forceMerge(1);
+
+	writer.commit();
+
+        return writer;
+    }
+
+    @Bean(name="indexSearcher")
+    @DependsOn("indexWriter")
+    public IndexSearcher getIndexSearcher() throws IOException {
+	try {
+	    return new IndexSearcher(DirectoryReader.open(getFSDirectory()));
+	} catch (IndexNotFoundException e) {
+	    LOG.warn("No valid Lucene index found at " + indexPath);
+	    return null;
+	}
+    }
+
+    @Bean(name="queryParser")
+    @DependsOn("analyzer")
+    public StandardQueryParser getQueryParser() throws IOException {
+        return new StandardQueryParser(getAnalyzer());
     }
 }
