4e4afcbaf981c89448886f63c741bc46182036b6
Markus Koskela <markus.koskela@cs.helsinki.fi>
2015-09-30 09:27:31 +0300

    Now supports keyword scores.

--- loggers/keylogger/plot_pickle.py
+++ loggers/keylogger/plot_pickle.py
@@ -1,25 +1,28 @@
 #!/opt/local/bin/python2.7
 
 import sys
+import math
 
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
+vindex = int(sys.argv[1])
+
 means = []
 for i in range(0,50):
     means.append([])
 
-for a in sys.argv[1:]:
-    print "Processing", a 
-    f = open(a) 
+for a in sys.argv[2:]:
+    print("Processing", a)
+    f = open(a, 'rb')
     x = pickle.load(f)
     for i,xx in enumerate(x):
-        if i<50:
+        if i<50 and not math.isnan(xx[vindex]):
             #print i,xx
-            means[i].append(xx[0])
+            means[i].append(xx[vindex])
     y = np.array(x)
-    plt.plot(y[:,0])
+    plt.plot(y[:,vindex])
 
 meanvector = np.zeros(50)
 for i,m in enumerate(means):
