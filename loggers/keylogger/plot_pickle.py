629e7b428547b92c754490d9ba6f50bccced7ee4
Markus Koskela <markus.koskela@cs.helsinki.fi>
2015-10-01 15:10:03 +0300

    Now saves the mean vector to disk.

--- loggers/keylogger/plot_pickle.py
+++ loggers/keylogger/plot_pickle.py
@@ -13,6 +13,9 @@ means = []
 for i in range(0,50):
     means.append([])
 
+first = True
+meanvectorfn = "mean_" + sys.argv[1] +  '_'
+
 for a in sys.argv[2:]:
     print("Processing", a)
     f = open(a, 'rb')
@@ -23,12 +26,21 @@ for a in sys.argv[2:]:
             means[i].append(xx[vindex])
     y = np.array(x)
     plt.plot(y[:,vindex])
+    if first:
+        first = False
+        parts = a.split("/")
+        meanvectorfn += parts[0]
+        if a.find("old") > -1:
+            meanvectorfn += "_old"
 
 meanvector = np.zeros(50)
 for i,m in enumerate(means):
     if len(m):
         #print i, len(m), type(m), m[0]
         meanvector[i] = float(sum(m))/len(m)
+
+print("Saved", meanvectorfn)
+np.save(meanvectorfn, meanvector)
+
 plt.plot(meanvector, linewidth=5, color='k')
-    
 plt.show()
