c0aaac2e0af212d6173571244a6282e2a9d93009
peacekeeper <markus@danubetech.com>
2017-06-06 11:47:38 +0200

    debug libsovrin loading

--- src/main/java/fi/hiit/dime/sovrin/SovrinService.java
+++ src/main/java/fi/hiit/dime/sovrin/SovrinService.java
@@ -51,8 +51,9 @@ public class SovrinService {
 
 		try {
 
+			LOG.info("Loading libsovrin: " + new File("./lib/libsovrin.so").getAbsolutePath());
 			if (! LibSovrin.isInitialized()) LibSovrin.init(new File("./lib/libsovrin.so"));
-		} catch (Exception ex) {
+		} catch (Throwable ex) {
 
 			LOG.warn(ex.getMessage(), ex);
 		}
